922e971588b5d511a9ffb9dcd376a34a0992f2fd
mefix23 <ismikshop@gmail.com>
2026-09-20 01:29:35 +0300

    Update profiles.js

--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { list, getById, create, update, myProfiles } = require('../controllers/profileController');
+const { list, getById, create, update, remove, myProfiles } = require('../controllers/profileController');
 const { validateProfileCreate } = require('../middleware/validate');
 const { requireAuth, optionalAuth } = require('../middleware/auth');
 
@@ -10,5 +10,6 @@ router.get('/mine', requireAuth, myProfiles);
 router.get('/:id', optionalAuth, getById);
 router.post('/', requireAuth, validateProfileCreate, create);
 router.put('/:id', requireAuth, validateProfileCreate, update);
+router.delete('/:id', requireAuth, remove);
 
 module.exports = router;
